b6bbb28a9cfd5fa595f7b131f5957666f6807b9b
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-26 19:21:23 +0900

    YML 설정 및 실습 / 2024-05-26

---
+++ src/main/java/com/yang/blog/test/TempControllerTest.java
@@ -0,0 +1,18 @@
+package com.yang.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+	
+	@GetMapping("/temp/home")
+	public String tempHome() {
+		System.out.println("tempHome()");
+		
+		// 파일리턴 기본경로 : src/main/resources/static
+		// 리턴명 : /home.html
+		// 풀경로 : src/main/resources/static/home.html
+		return "/home.html";
+	}
+}
