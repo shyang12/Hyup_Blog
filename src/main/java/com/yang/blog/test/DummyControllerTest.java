4e89a937d948a642564736e8232c6b2330039935
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-27 16:01:21 +0900

    id 로 Select 테스트 / 2024-05-27

--- src/main/java/com/yang/blog/test/DummyControllerTest.java
+++ src/main/java/com/yang/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.yang.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,27 @@ public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	// {id} 주소로 파마레터를 전달 받을 수 있음
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		// user/4 를 찾으면 내가 데이터베이스에서 못찾아오게 되면  user가 null이 될 것 아냐?
+		// 그럼 return null이 리턴 되잖아.. 그럼 프로그램에 문제가 있지 않겠니??
+		// Optional로 너의 User 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return 해!
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException> () {
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+			}
+		});
+		// 요청 : 웹브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브러우저에게 던져주빈다
+		return user;
+	}
 
 	// http의 body에 username, password, email 데이터를 가지고 (요청)
 	@PostMapping("/dummy/join")
