3c2020992824fb74151cb0e2e50379bd8b7a302e
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-06-02 20:16:35 +0900

    ResponseDto 수정 / 2024-06-02

--- src/main/java/com/yang/blog/controller/api/UserApiController.java
+++ src/main/java/com/yang/blog/controller/api/UserApiController.java
@@ -22,7 +22,7 @@ public class UserApiController {
 		System.out.println("UserApiController : Save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 된다.
 		user.setRole(RoleType.USER);
-		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK, 1);
+		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
