b34672b58554ba69da969f435b813e28c5b48c43
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-06-04 16:41:17 +0900

    시큐리티 로그인을 위한 코드 수정 / 2024-06-04

--- src/main/java/com/yang/blog/controller/UserController.java
+++ src/main/java/com/yang/blog/controller/UserController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
 	
-	@GetMapping("/user/joinForm")
+	@GetMapping("/joinForm")
 	public String joinForm() {
 		
 		return "user/joinForm";
 	}
 	
-	@GetMapping("/user/loginForm")
+	@GetMapping("/loginForm")
 	public String loginForm() {
 		
 		return "user/loginForm";
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -23,14 +23,14 @@ let index = {
 			$.ajax({
 				// 회원가입 수행 요청(100초 가정)
 				type: "POST",
-				url: "/blog/api/user",
+				url: "/api/user",
 				data: JSON.stringify(data), // JSON 문자열 -> http body 데이터
 				contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
 				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 String (생긴게 JSON이라면) => javascript오브젝트로 변경
 			}).done(function(resp) {
 				alert("회원가입이 완료되었습니다.");
 				// console.log(resp);
-				location.href="/blog";
+				location.href="/";
 			}).fail(function(error) {
 				alert(JSON.stringify(error));
 			}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
@@ -50,14 +50,14 @@ let index = {
 			$.ajax({
 				// 회원가입 수행 요청(100초 가정)
 				type: "POST",
-				url: "/blog/api/user/login",
+				url: "/api/user/login",
 				data: JSON.stringify(data), // JSON 문자열 -> http body 데이터
 				contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
 				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 String (생긴게 JSON이라면) => javascript오브젝트로 변경
 			}).done(function(resp) {
 				alert("로그인이 완료되었습니다.");
 				// console.log(resp);
-				location.href="/blog";
+				location.href="/";
 			}).fail(function(error) {
 				alert(JSON.stringify(error));
 			}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
