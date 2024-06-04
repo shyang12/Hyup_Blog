aacc5908c186183e07351d2fb86761c808b9c708
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-06-04 17:00:50 +0900

    시큐리티 로그인 구현 / 2024-06-04

--- src/main/java/com/yang/blog/controller/api/UserApiController.java
+++ src/main/java/com/yang/blog/controller/api/UserApiController.java
@@ -19,8 +19,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
 
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
@@ -31,14 +29,13 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
-		System.out.println("UserApiController : login 호출됨");
-		User principal = userService.로그인(user); // principal (접근주체)
-		
-		if(principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+	/*
+	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
+	 * login(@RequestBody User user, HttpSession session) {
+	 * System.out.println("UserApiController : login 호출됨"); User principal =
+	 * userService.로그인(user); // principal (접근주체)
+	 * 
+	 * if(principal != null) { session.setAttribute("principal", principal); }
+	 * return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); }
+	 */
 }
